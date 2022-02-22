a253a4b0354b1c0baa590a5da6cca9ced1fa5c2f
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-02-22 17:04:18 -0300

    add main movie request

--- src/pages/api/TMDB.js
+++ src/pages/api/TMDB.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
-const APY_KEY = 'c945acef578ef380a52477b252704a79';
-const APY_BASE = 'https://api.themoviedb.org/3/';
+const API_KEY = 'c945acef578ef380a52477b252704a79';
+const API_BASE = 'https://api.themoviedb.org/3/';
 
 const Request = async (link) => {
-    let url = `${APY_BASE}${link}`;
+    let url = `${API_BASE}${link}`;
     let response = await fetch(url);
 
     response = response.json();
@@ -11,23 +11,33 @@ const Request = async (link) => {
 }
 
 export default {
+
+    GetRandomMovie: async (id) => {
+        return await Request(`movie/${id}?api_key=${API_KEY}`)
+    },
+
     GetListMovies: async () => {
         
         const list =[
             {
                 name: 'trending',
                 title: 'Trending',
-                list: await Request(`trending/movie/week?api_key=${APY_KEY}`)
+                list: await Request(`trending/movie/week?api_key=${API_KEY}`)
             },
             {
                 name: 'disney',
                 title: 'Disney Movies',
-                list: await Request(`discover/movie?sort_by=popularity.desc&with_companies=2&api_key=${APY_KEY}`)
+                list: await Request(`discover/movie?sort_by=popularity.desc&with_companies=2&api_key=${API_KEY}`)
             },
             {
                 name: 'lucasfilm',
                 title: 'Lucasfilm',
-                list: await Request(`discover/movie?sort_by=popularity.desc&with_companies=1&api_key=${APY_KEY}`)
+                list: await Request(`discover/movie?sort_by=popularity.desc&with_companies=1&api_key=${API_KEY}`)
+            },
+            {
+                name: 'fox',
+                title: 'Fox Movies',
+                list: await Request(`discover/movie?sort_by=popularity.desc&with_companies=25&api_key=${API_KEY}`)
             }
         ]
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,23 @@ import style from "../styles/Home.module.css";
 export default function Index(){
 
   const [list , setList] = useState([])
+  const [randomMovie , setRandomMovie] = useState([])
   
   useEffect(() => {
     const LoadAll = async () => {
       let tmdb_res = await TMDB.GetListMovies();
       setList(tmdb_res);
+
+      let randomNumber = Math.floor(Math.random() * (tmdb_res[0].list.results.length - 1));
+      let idMovie = tmdb_res[0].list.results[randomNumber].id;
+      let getMovie = await TMDB.GetRandomMovie(idMovie);
+      setRandomMovie(getMovie);
     }
 
     LoadAll()
   },[])
 
-  console.log(list);
+  console.log(randomMovie);
   
   return(
     <main>
