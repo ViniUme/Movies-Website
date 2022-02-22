e77975939d07a45a79c67d0546e4a519b697b0d7
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-02-22 14:23:47 -0300

    add animation list

--- src/pages/components/ListArea.js
+++ src/pages/components/ListArea.js
@@ -1,22 +1,47 @@
+import {useState} from 'react';
 import style from "../../styles/ListArea.module.css";
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';;
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 
 export default function ListArea({info}){
+
+    const [marginLeft , setMarginLeft] = useState(0)
+
     return(
         <>
         <h3 className={style.title}>{info.title}</h3>
         
         <div className={style.list_div}>
 
-            <div>
-                <NavigateBeforeIcon />
-            </div>
-            <div>
-                <NavigateNextIcon />
+            <div className={style.NavBefore} onClick={() => {
+                if(marginLeft <= 0){
+                    return
+                }
+                else{
+                    let marginCount = marginLeft - 166;
+                    setMarginLeft(marginCount);
+                }
+            }}>
+                <NavigateBeforeIcon style={{fontSize: 50}} />
             </div>
 
-            <div className={style.imgs_div}>
+
+            <div className={style.NavNext} onClick={() => {
+                    let windowSize = window.innerWidth;
+                    let listSize = info.list.results.length * 166;
+                    console.log(windowSize);
+                    if(marginLeft >= listSize){
+                        return
+                    }
+                    else{
+                        let marginCount = marginLeft + 166;
+                        setMarginLeft(marginCount);
+                    }
+                }}>
+                <NavigateNextIcon style={{fontSize: 50}} />
+            </div>
+
+            <div className={style.imgs_div} style={{marginLeft: `-${marginLeft}px`}}>
 
                 {info.list.results.map((item , key) => {
                     return(
